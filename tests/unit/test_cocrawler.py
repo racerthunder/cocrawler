f62ca6315ac81e1429a3c70b57ff1018bf185c51
brc <alwaysontapz@gmail.com>
2018-06-12 11:28:56 +0300

    test files

--- tests/unit/test_cocrawler.py
+++ tests/unit/test_cocrawler.py
@@ -54,3 +54,41 @@ def test_cocrawler(capsys):
 
     assert err == ''
     assert len(out) >= 200  # not a very good test, but at least it is something
+
+
+def misc():
+    config.config(None, None, confighome=False)
+
+        # we have to get around the useragent checks
+    config.write('pytest', 'UserAgent', 'MyPrefix')
+    config.write('http://example.com/pytest-test-cocrawler.py', 'UserAgent', 'URL')
+    # and configure url_allowed
+    config.write('AllDomains', 'Plugins', 'url_allowed')
+
+    crawler = cocrawler.Crawler()
+
+    crawler.add_url(0, {'url': URL('http://tut.by/')})
+    crawler.add_url(0, {'url': URL('http://habr.com/')})
+
+    assert crawler.qsize == 3
+
+    f = tempfile.NamedTemporaryFile(delete=False)
+    name = f.name
+
+    with open(name, 'wb') as f:
+        crawler.save(f)
+    assert crawler.qsize == 0
+
+    crawler.add_url(0, {'url': URL('http://example4.com/')})
+    assert crawler.qsize == 1
+
+    with open(name, 'rb') as f:
+        crawler.load(f)
+
+    assert crawler.qsize == 3
+
+    os.unlink(name)
+    assert not os.path.exists(name)
+
+if __name__ == '__main__':
+    misc()
